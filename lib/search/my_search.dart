cbbc3f92876561f284458b326e716a47e4caa1d2
Pawan Kumar <pk89844@gmail.com>
2024-04-30 00:10:43 +0530

    Search Added

--- lib/search/my_search.dart
+++ lib/search/my_search.dart
@@ -11,6 +11,7 @@ class MySearch extends StatefulWidget {
 class _MySearchState extends State<MySearch> {
   final TextEditingController _searchcontroller = TextEditingController();
   String productName = '';
+  List<UserDataModel> _searched = [];
 
   @override
   Widget build(BuildContext context) {
@@ -19,29 +20,53 @@ class _MySearchState extends State<MySearch> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            alignment: Alignment.center,
-            child: Text('Searched Product: $productName'),
-          ),
+          // Container(
+          //   alignment: Alignment.center,
+          //   child: Text('Searched Product: $productName'),
+          // ),
           TextField(
             controller: _searchcontroller,
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                var data = UserData.userData
+                    .where((prod) => prod.productName
+                        .toLowerCase()
+                        .startsWith(value.toLowerCase()))
+                    .toList();
+                if (!_searched.contains(data[0])) {
+                  _searched.addAll(data);
+                }
+                // setState(() {});
+              } else {
+                _searched.clear();
+              }
+              setState(() {});
+            },
             decoration: const InputDecoration(
                 labelText: 'Search Here',
                 border: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black))),
-            onSubmitted: (value) {
-              var data = UserData.userData
-                  .where((prod) => prod.productName
-                      .toLowerCase()
-                      .startsWith(value.toLowerCase()))
-                  .toList();
+            // onSubmitted: (value) {
 
-              for (var i = 0; i < data.length; i++) {
-                print(data[i].productName);
-              }
-              // print('Data: ${data[0].productName}');
-            },
+            //   // for (var i = 0; i < data.length; i++) {
+            //   //   print(data[i].productName);
+            //   // }
+            //   // print('Data: ${data[0].productName}');
+            // },
           ),
+          if (_searched.isNotEmpty) ...[
+            ListView.builder(
+              shrinkWrap: true,
+              primary: false,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: _searched.length,
+              itemBuilder: (context, i) {
+                return Card(
+                  child: Text(_searched[i].productName),
+                );
+              },
+            )
+          ]
         ],
       ),
     );
